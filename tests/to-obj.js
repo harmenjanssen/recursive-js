2f55cab52817edb62bf1943fad56c3c6415d1f5f
Harmen Janssen <harmen@grrr.nl>
2017-08-20 16:34:51 +0200

    Added toObj

---
+++ tests/to-obj.js
@@ -0,0 +1,17 @@
+const toObj = require("../lib/to-obj");
+const assert = require("assert");
+
+describe("reverse", () => {
+  it("creates an object when given an array of keys and values", () =>
+    assert.deepEqual(toObj(["foo", "data", "baz"], ["bar", [1, 2, 3], true]), {
+      foo: "bar",
+      data: [1, 2, 3],
+      baz: true
+    }));
+
+  it("ignores superfluous values", () =>
+    assert.deepEqual(toObj(["foo", "data"], ["bar", [1, 2, 3], true]), {
+      foo: "bar",
+      data: [1, 2, 3]
+    }));
+});
